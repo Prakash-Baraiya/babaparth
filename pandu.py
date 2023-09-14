0fa73f5baf86733f3ef04ce11bc5a4b55594be49
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-09-14 16:47:19 +0530

    Update pandu.py

--- pandu.py
+++ pandu.py
@@ -1,28 +1,69 @@
 import requests
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
 
-# Replace this with your token (Smaller One)
-token = "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6OTI1MzU1NzksIm9yZ0lkIjo0MDMwOTUsInR5cGUiOjEsIm1vYmlsZSI6IjkxNjM1OTE0NjE0NSIsIm5hbWUiOiJQcmFrYXNoIEJhcmFpeWEiLCJlbWFpbCI6InByYWthc2gxNTEwODNAZ21haWwuY29tIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJkZWZhdWx0TGFuZ3VhZ2UiOiJFTiIsImNvdW50cnlDb2RlIjoiSU4iLCJjb3VudHJ5SVNPIjoiOTEiLCJ0aW1lem9uZSI6IkdNVCs1OjMwIiwiaXNEaXkiOmZhbHNlLCJvcmdDb2RlIjoib3hwYmgiLCJmaW5nZXJwcmludElkIjoiYTg0MjNkYjFlZjE5MjI3ZTMyOGFmNGEwMGRlODJlMTEiLCJpYXQiOjE2OTQwNzk1MjYsImV4cCI6MTY5NDY4NDMyNn0.3EatpR80XlzD2q9pImEnvYXieV3SfwckUExG_Y-4NtLk6CSm_dkKPfRKynp-Ed3F"
+# Define the token for making requests to the external API
+external_api_token = "YOUR_EXTERNAL_API_TOKEN"
 
-response = requests.post(
-    "https://learnyst.devsrajput.com/free", 
-    data={
-        "link":token,
-    }
-)
+# Define the token for your Telegram bot
+telegram_bot_token = "YOUR_TELEGRAM_BOT_TOKEN"
 
-if response.status_code == 200:
-    try:
-        data = response.json()
-    except ValueError as e:
-        print("Failed to parse JSON data from the API response.")
-        exit()
-else:
-    print("Request Failed! reasons:\n\t1). Token Expired\n\t2). API Not Working")
-    exit()
+# Define the states for the conversation
+START, PROCESS_TEXT = range(2)
 
-data = response.json()
-name = data["TITLE"]
-link = data["MPD"]
-keys = data["KEY_STRING"]
+# Function to send the final output to the user
+def send_final_output(update: Update, context: CallbackContext) -> int:
+    user_text = update.message.text
+    response = requests.post(
+        "https://learnyst.devsrajput.com/free", 
+        data={
+            "link": external_api_token,
+        }
+    )
+    
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            name = data["TITLE"]
+            link = data["MPD"]
+            keys = data["KEY_STRING"]
+            final_output = f"{name}\n{link}\n{external_api_token}\n\n{keys}"
+            update.message.reply_text(final_output)
+        except ValueError as e:
+            update.message.reply_text("Failed to process data from the API response.")
+    else:
+        update.message.reply_text("Request Failed! Possible reasons:\n1). Token Expired\n2). API Not Working")
+    
+    return ConversationHandler.END
 
-print(f"{name}\n{link}\n{token}\n\n{keys}")
+# Function to start the conversation
+def start(update: Update, context: CallbackContext) -> int:
+    update.message.reply_text("Welcome! Please send me the text to process.")
+    return PROCESS_TEXT
+
+# Function to cancel the conversation
+def cancel(update: Update, context: CallbackContext) -> int:
+    update.message.reply_text("Conversation cancelled.")
+    return ConversationHandler.END
+
+def main():
+    updater = Updater(telegram_bot_token, use_context=True)
+    dispatcher = updater.dispatcher
+
+    # Create a conversation handler
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            PROCESS_TEXT: [MessageHandler(Filters.text & ~Filters.command, send_final_output)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
+    # Add the conversation handler to the dispatcher
+    dispatcher.add_handler(conv_handler)
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
